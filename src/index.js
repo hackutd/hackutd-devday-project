3e916c9d3872fd840529f0886d28d6b301e82795
Kevin Ge <ge.kevin.a@gmail.com>
2023-07-28 17:58:34 -0700

    make pages folder and fix video aspect ratio

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Home from "./Home";
-import Video from "./Video";
+import Home from "./pages/Home";
+import Video from "./pages/Video";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,16 +1,17 @@
 import { useParams } from "react-router-dom";
-import { ReactComponent as PlayIcon } from "./assets/play-icon.svg";
-import { ReactComponent as ThumbsUpIcon } from "./assets/thumbs-up.svg";
-import { ReactComponent as ThumbsDownIcon } from "./assets/thumbs-down.svg";
-import Navbar from "./components/Navbar";
+import { ReactComponent as PlayIcon } from "../assets/play-icon.svg";
+import { ReactComponent as ThumbsUpIcon } from "../assets/thumbs-up.svg";
+import { ReactComponent as ThumbsDownIcon } from "../assets/thumbs-down.svg";
+import Navbar from "../components/Navbar";
 import { useState } from "react";
-import Spinner from "./components/Spinner";
+import Spinner from "../components/Spinner";
 
 export default function Video() {
   const [video, setVideo] = useState({
     title: "Cowboy Bebop",
     likes: 100,
     dislikes: 20,
+    thumbnail: "https://i.imgur.com/Ea5abYt.png",
   });
   const [loading, setLoading] = useState(false); // TODO: set this to true when fetching video.
   const { id } = useParams();
@@ -24,10 +25,16 @@ export default function Video() {
   ) : (
     <div className="w-screen min-h-screen bg-primary text-text-primary overflow-x-hidden">
       <Navbar />
-      <section className="w-full px-[5%] py-12 flex flex-col gap-4 justify-center">
+
+      <section className="w-full px-[16vw] py-12 flex flex-col gap-4 justify-center">
         <section>
-          <article className="h-[550px] bg-black flex items-center justify-center">
-            <PlayIcon className="w-12 h-12" />
+          <article className="aspect-video bg-black relative rounded-xl">
+            <img
+              className="w-full h-full"
+              src={video.thumbnail}
+              alt={video.title}
+            />
+            <PlayIcon className="w-12 h-12 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
           </article>
           <article className="flex justify-between font-bold mt-4">
             <h2 className="text-3xl">{video.title}</h2>

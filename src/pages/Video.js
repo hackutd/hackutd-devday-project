864b4c67cc7f382e18bcf83ae193ed63dfba3697
Kevin Ge <ge.kevin.a@gmail.com>
2023-07-29 19:18:20 -0700

    feat: add api call for video

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,24 +1,28 @@
 import { useParams } from "react-router-dom";
-import { ReactComponent as PlayIcon } from "../assets/play-icon.svg";
 import { ReactComponent as ThumbsUpIcon } from "../assets/thumbs-up.svg";
 import { ReactComponent as ThumbsDownIcon } from "../assets/thumbs-down.svg";
 import Navbar from "../components/Navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Spinner from "../components/Spinner";
+import { apiBaseURL } from "../constants";
 
 export default function Video() {
-  const [video, setVideo] = useState({
-    title: "Cowboy Bebop",
-    likes: 100,
-    dislikes: 20,
-    thumbnail: "https://i.imgur.com/Ea5abYt.png",
-  });
-  const [loading, setLoading] = useState(false); // TODO: set this to true when fetching video.
+  const [video, setVideo] = useState(null);
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
 
-  // TODO: fetch video by ID here.
+  useEffect(() => {
+    fetch(`${apiBaseURL}/${id}`)
+      .then((r) => {
+        console.log(r);
+        return r;
+      })
+      .then((r) => r.json())
+      .then((video) => setVideo(video))
+      .finally(() => setLoading(false));
+  }, [id]);
 
-  return loading ? (
+  return loading || !video ? (
     <div className="w-screen h-screen bg-primary flex items-center justify-center">
       <Spinner />
     </div>
@@ -29,16 +33,20 @@ export default function Video() {
       <section className="w-full px-[16vw] py-12 flex flex-col gap-4 justify-center">
         <section>
           <article className="aspect-video bg-black relative rounded-xl">
-            <img
+            <video
               className="w-full h-full"
-              src={video.thumbnail}
-              alt={video.title}
-            />
-            <PlayIcon className="w-12 h-12 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+              controls
+              poster={video.thumbnailUrl}
+              autoPlay
+              muted
+            >
+              <source src={video.videoUrl} type="video/mp4" />
+            </video>
           </article>
           <article className="flex justify-between font-bold mt-4">
             <h2 className="text-3xl">{video.title}</h2>
             <div className="flex gap-2 items-center">
+              {/* TODO: add firebase call for this */}
               {video.likes}
               <ThumbsUpIcon className="w-8 h-8" />
               <ThumbsDownIcon className="w-8 h-8" />
